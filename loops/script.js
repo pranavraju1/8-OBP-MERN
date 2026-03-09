5ca2ae11aa5677026ae227f11dd7b8958f4d52c8
pranavraju1 <chatwithsoonu@gmail.com>
2026-03-09 08:01:00 +0530

    6-3-26 push

---
+++ loops/script.js
@@ -0,0 +1,36 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+// let i = 1;
+// let result
+// while ( i <= 5 ) {
+//     // result = i
+//     console.log(i);
+//     // i = i+1;
+//     i++;
+// }
+
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+
+// let i = 5;
+// while(i>=1){
+//     console.log(i)
+//     i--;
+// }
+
+// print all even numbers from 20 to 40 (both included)
+
+// let i = 20;
+// while(i<=40){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+//     i++;
+// }
