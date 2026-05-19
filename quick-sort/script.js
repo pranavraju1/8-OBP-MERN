a5a1f3f36ccfbde3f44277b52c2a6a3acefde51b
pranavraju1 <chatwithsoonu@gmail.com>
2026-05-19 16:49:25 +0530

    19-5-26 push

--- quick-sort/script.js
+++ quick-sort/script.js
@@ -29,39 +29,39 @@ console.log(quickSort(arr));
 
 
 
-// // Inplace quickSort()
-// function partition(arr, low, high) {
-//   const pivot = arr[high];      // 1) choose pivot
-//   let i = low - 1;              // 2) boundary of "smaller than pivot" region
+// Inplace quickSort()
+function partition(arr, low, high) {
+  const pivot = arr[high];      // 1) choose pivot
+  let i = low - 1;              // 2) boundary of "smaller than pivot" region
 
 
-//   for (let j = low; j <= high - 1; j++) {
-//     if (arr[j] < pivot) {       // 3) element belongs to "smaller" region
-//       i++;                      // move boundary right
-//       [arr[i], arr[j]] = [arr[j], arr[i]];   // swap into that region
-//     }
-//   }
+  for (let j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) {       // 3) element belongs to "smaller" region
+      i++;                      // move boundary right
+      [arr[i], arr[j]] = [arr[j], arr[i]];   // swap into that region
+    }
+  }
 
 
-//   // 4) put pivot in its final position (right after "smaller" region)
-//   [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
-//   return i + 1;                 // 5) return pivot index
-// }
+  // 4) put pivot in its final position (right after "smaller" region)
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+  return i + 1;                 // 5) return pivot index
+}
 
 
-// function quickSort(arr, low = 0, high = arr.length - 1) {
-//   if (low >= high) return;      // base case: 0 or 1 element
+function quickSort(arr, low = 0, high = arr.length - 1) {
+  if (low >= high) return;      // base case: 0 or 1 element
 
 
-//   const pi = partition(arr, low, high);  // 6) partition
+  const pi = partition(arr, low, high);  // 6) partition
 
 
-//   // 7) recursively sort left and right parts (in place)
-//   quickSort(arr, low, pi - 1);
-//   quickSort(arr, pi + 1, high);
-// }
+  // 7) recursively sort left and right parts (in place)
+  quickSort(arr, low, pi - 1);
+  quickSort(arr, pi + 1, high);
+}
 
 
-// let arr = [8, 3, 1, 7, 0, 10, 2]
-// quickSort(arr)
-// console.log(arr)
+let arr = [8, 3, 1, 7, 0, 10, 2]
+quickSort(arr)
+console.log(arr)
